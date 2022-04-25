1cd90afacac4ed6dd6b3e242058d24a57af8862a
Enigma Swan Adams <eswanadams@alaska.edu>
2022-04-25 01:52:26 -0800

    Add project files.

---
+++ MP1/main.cpp
@@ -0,0 +1,31 @@
+/*
+* CS 202 homework 5
+* 4/25/2022
+*
+* file containing main for the
+* Main Program 1: Template read() write()
+* just the code we need to compile from the 
+* homework handout
+*/
+
+
+
+int main() {
+
+/*
+	ofstream ofile("numbers.dat");
+	double d = 13.3;
+	myWrite(ofile, d);
+	int x = 5;
+	myWrite(ofile, x);
+	…
+		ifstream ifile("numbers.dat");
+	double readd;
+	int readx;
+
+	myRead(ifile, readd);
+	myRead(ifile, readx);
+	*/
+
+	return 0; 
+}
---
+++ MP1/myreadwrite.h
@@ -0,0 +1,19 @@
+/*
+* CS 202 homework 5
+* 4/25/2022
+*
+* header for 
+* Main Program 1: Template read() write()
+* 
+* contains the the templated function defintiions 
+*/
+
+
+
+#ifndef MYREADWRITE_H_
+#define MYREADWRITE_H_
+
+
+
+#endif /* MYREADWRITE_H_ */
+
